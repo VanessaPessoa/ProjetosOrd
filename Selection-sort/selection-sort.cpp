d569705c38cab7eab9f848784c1424f40f9330a5
VanessaPessoa <vanessalima@cc.ci.ufpb.br>
2020-02-14 21:29:48 -0300

    correção selection sort

--- Selection-sort/selection-sort.cpp
+++ Selection-sort/selection-sort.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void troca(int *A, int *B){
-    int *temp = A;
-    A = B;
-    B = temp;
+void troca(int i, int j){
+    int temp = i;
+    i = j;
+    j = temp;
 }
 
-void selectionSort(int *A, int n){
-    int i, j, min;
-    for(i = 0; i < n - 1; i++){
-        min = i;
-        for(j = i+1; j < n; j++){
-            if(A[j] < A[min]){
-                min = j;
-            }
-        }
-        troca(&A[min], &A[i]);
+void selectionSort (int A[],int n) {
+  int i, j, i_min, aux;
+  for (i = 0; i < (n- 1); i++) {
+    /* O minimo é o primeiro número não ordenado ainda */
+    i_min = i;
+    for (j = i+1; j < n; j++) {
+      /* Caso tenha algum numero menor ele faz a troca do minimo*/
+      if (A[j] < A[i_min]) {
+        i_min = j;
+      }
     }
-
+    /* Se o minimo for diferente do primeiro numero não ordenado ele faz a troca para ordena-los*/
+    if (i != i_min) {
+      aux = A[i];
+      A[i] = A[i_min];
+      A[i_min] = aux;
+    }
+  }
 }
-
 void printArray(int A[], int n){
     int i;
     for(i = 0; i < n; i++){
@@ -32,10 +37,10 @@ void printArray(int A[], int n){
 int main(){
     int A[] = { 20, 12, 15, 3, 1, 32, 10, 40, 100, 35};
     int n = sizeof(A)/ sizeof(A[0]);
-
+ cout << n << endl;
     cout << " Antes do selection sort" << endl;
     printArray(A,n);
-    selectionSort(A, n);
+    selectionSort(A,n);
     cout<< "Depois do selection sort" << endl;
     printArray(A,n);
 
